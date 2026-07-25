a428eb28abebd3034c072c805eb563e7b676b24b
murjkt <murjkt@gmail.com>
2026-07-26 07:39:07 +0800

    config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // URL Web App Google Apps Script (backend data desa).
 // Ganti nilai ini kalau suatu saat deploy ulang Apps Script dan dapat URL baru.
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx6_ddJ3NAxKx0OYf3bPvgi5dduPE3EYjSxFVKweLn3OWC6xDD6VH9WAUnbsYsI1dM2/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzVWms8D_J7XmeoVD03bUthVDt5Nadd3yPPw5u4Q95ZkGxSJThH0Wb0F-QihoUjRnSW/exec";
