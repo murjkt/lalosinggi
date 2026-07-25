5c8e69a71c193ce6ea0a158fb6f313099ffc262a
murjkt <murjkt@gmail.com>
2026-07-26 07:09:54 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// URL Web App Google Apps Script (backend data desa).
+// Ganti nilai ini kalau suatu saat deploy ulang Apps Script dan dapat URL baru.
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx6_ddJ3NAxKx0OYf3bPvgi5dduPE3EYjSxFVKweLn3OWC6xDD6VH9WAUnbsYsI1dM2/exec";
